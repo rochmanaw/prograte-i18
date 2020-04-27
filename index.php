d8c6457a6034a4a18cd520620434189b861d4b26
Rochman Ari Wibowo <r.wibowo@eksad.com>
2020-04-27 09:56:46 +0700

    Add existing file

---
+++ index.php
@@ -0,0 +1,213 @@
+<?php
+?>
+
+<link rel="stylesheet" href="stylesheet.css" type="text/css">
+<link rel="stylesheet" href="flix.css" media="screen">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <script src="flix.js"></script>
+    <script src="anime.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="header.js"></script>
+<html lang="en">
+<head>
+<title>Progate - I18</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<header>
+        <div class="menu-toggle" id="hamburger">
+            <i class="fas fa-bars"></i>
+        </div>
+        <div class="overlay"></div>
+        <div class="container">
+            <nav>
+                <h1 class="brand"><a href="index.php" style="color:white;">Pro<span>ga</span>te</a></h1>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+</header>
+<body>
+
+
+<p>&nbsp;</p><p>&nbsp;</p> 
+
+<section class="hero">
+    <div class="background-image" style="background-image: url(assets/images/hero.jpg);"></div>
+    <div class="animate seven">
+        <span>W</span><span>e</span><span>l</span><span>c</span><span>o</span><span>m</span><span>e</span><span>!</span> &nbsp;
+    </div>
+    <div class="animate five">
+        <span>T</span><span>o</span><span>&nbsp;</span><span>O</span><span>u</span><span>r</span><span>&nbsp;</span><span>S</span><span>i</span><span>t</span><span>e</span>
+    </div>
+    <div class="animate six">
+        <span>H</span><span>e</span><span>l</span><span>l</span><span>o</span> &nbsp;
+        <span>E</span><span>v</span><span>e</span><span>r</span><span>y</span><span>o</span><span>n</span><span>e</span><span>&nbsp;</span><span>^</span><span>_</span><span>^</span>
+    </div>
+</section>
+<div class="support-grid"></div>
+  <div class="band">
+    <div class="item-1">
+          <a href="" class="card">
+            <div class="thumb" style="background-image: url(hero2.jpg);"></div>
+            <article>
+              <h1>MEETS OUR TEAM</h1>
+            </article>
+          </a>
+    </div>
+    <div class="item-2">
+          <a href="" class="card">
+            <div class="thumb" style="background-image: url(assets/images/photo1.jpg);"></div>
+            <article>
+              <h1>Rochman Ari Wibowo</h1>
+              <p style="padding-top:20px;">How to Create a “Stranger Things” Text Effect</p>
+              <span>Team I-18</span>
+            </article>
+          </a>
+    </div>
+    <div class="item-3">
+      <a href="" class="card">
+        <div class="thumb" style="background-image: url(assets/images/photo2.jpg);"></div>
+        <article>
+          <h1>Sigit Ageng Utama</h1>
+          <p style="padding-top:20px;">Welcome to our monthly feature of fantastic tutorial results! </p>
+          <span>Team I-18</span>
+        </article>
+      </a>
+    </div>
+    <div class="item-4">
+          <a href="" class="card">
+            <div class="thumb" style="background-image: url(assets/images/photo3.jpg);"></div>
+            <article>
+              <h1>Shanty Nurhaliza</h1>
+              <p style="padding-top:20px;">We’ll be putting things together making its way to touch down.</p>
+              <span>Team I-18</span>
+            </article>
+          </a>
+    </div>
+    <div class="item-5">
+          <a href="" class="card">
+            <div class="thumb" style="background-image: url(assets/images/photo1.jpg);"></div>
+            <article>
+                <h1>Budiman</h1>
+              <p style="padding-top:20px;">We’ll be putting things together making its way to touch down.</p>
+              <span>Team I-18</span>
+            </article>
+          </a>
+    </div>
+    <div class="item-6">
+      <a href="" class="card">
+        <div class="thumb" style="background-image: url(assets/images/photo2.jpg);"></div>
+        <article>
+            <h1>Cahyono</h1>
+              <p style="padding-top:20px;">Making its way to touch down.</p>
+              <span>Team I-18</span>
+        </article>
+      </a>
+    </div>
+    <div class="item-7">
+      <a href="" class="card">
+        <div class="thumb" style="background-image: url(assets/images/photo1.jpg);"></div>
+        <article>
+            <h1>Andi</h1>
+              <p style="padding-top:20px;">Feature of fantastic results!</p>
+              <span>Team I-18</span>
+        </article>
+      </a>
+    </div>
+</div>
+<p>&nbsp;</p><p>&nbsp;</p> 
+<section class="features">
+    <h3 class="title">Features and services</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed aliquam dictum sapien, id sagittis augue malesuada eu.</p>
+    <hr>
+
+    <ul class="grid">
+        <li>
+            <i class="fa fa-camera-retro"></i>
+            <h4>Photography</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices vitae pulvinar velit.</p>
+        </li>
+        <li>
+            <i class="fa fa-cubes"></i>
+            <h4>Web Development</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices vitae pulvinar velit.</p>
+        </li>
+        <li>
+            <i class="fa fa-newspaper-o"></i>
+            <h4>Content Editing</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices vitae pulvinar velit.</p>
+        </li>
+    </div>
+</section>
+<p>&nbsp;</p><p>&nbsp;</p> 
+<div class="extra2">
+  <h1>
+    What We Have Done
+  </h1>
+        <p>
+        Listen. Explore. Deliver! As an agile web development with rails we solve hard problems with our
+        </p>
+        <p>
+         clients everyday and we love to create their great ideas into the stunning functionally products.Find
+        </p>
+        <p>
+          out more about our past work to see how we had help give solutions to our clients like you.
+        </p>
+        <p>&nbsp;</p><p>&nbsp;</p>    
+    <div>
+        <span class="btn message">Start</span>
+    </div>
+</div>
+<p>&nbsp;</p><p>&nbsp;</p> 
+<section class="client">
+<h3 class="title">Our Client</h3>
+</section>
+
+<!-- Flickity HTML init -->
+<div class="carousel js-flickity">
+  <!-- images from unsplash.com -->
+  <div class="carousel-cell">
+    <a href="https://www.garuda-indonesia.com/" target="_blank">
+        <img src="logo1.png">
+    </a>
+  </div>
+  <div class="carousel-cell">
+    <a href="https://www.pertamina.com/" target="_blank">
+        <img src="logo2.png">
+    </a>
+  </div>
+  <div class="carousel-cell">
+    <a href="https://www.aws.amazon.com/" target="_blank">
+        <img src="logo3.png">
+    </a>
+  </div>
+  <div class="carousel-cell">
+    <a href="https://www.panasonic.com/" target="_blank">
+        <img src="logo4.jpg">
+    </a>
+  </div>
+</div>
+<p>&nbsp;</p><p>&nbsp;</p> 
+
+<footer>
+  <!-- Footer legal -->
+  <section class="ft-legal">
+    <ul class="ft-legal-list">
+      <li><a href="#">Terms &amp; Conditions</a></li>
+      <li><a href="#">Privacy Policy</a></li>
+      <li>&copy; 2020 Copyright Progate - Kelompok I18.</li>
+    </ul>
+  </section>
+</footer>
+</body>
+</html>
+
+<?php
+?>
